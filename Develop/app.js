db610035ca86a2574c429e7270cc91f1f8caa28b
jessny <jessnyjoseph@gmail.com>
2021-01-22 00:20:43 -0800

    added initial structure to readme

--- Develop/app.js
+++ Develop/app.js
@@ -11,11 +11,7 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 var globalAnswers;
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-// email, id, company role
-
-// Create Prompt
+// Prompt to gather info about team members
 const employeePrompt = () => { 
     return inquirer.prompt([
         {
@@ -99,9 +95,6 @@ employeePrompt();
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
 
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
