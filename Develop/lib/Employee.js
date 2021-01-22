a54362173be5b578a38acdbe82dcd12c8c045bea
jessny <jessnyjoseph@gmail.com>
2021-01-21 17:57:39 -0800

    finished employee class

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -4,18 +4,19 @@ class Employee {
         this.name = name;
         this.id = id;
         this.email = email;
-        this.role = employee;
     }
-    getName(name) {
-
+    getName() {
+        return this.name;
     }
     getId() {
-
+        return this.id;
     }
     getEmail() {
-
+        return this.email;
     }
     getRole() {
-        return employee;
+        return "Employee";
     }
 }
+
+module.exports = Employee;
