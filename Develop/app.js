0b28148c5bd26f560bedaad5bb65e94de8bd9880
jessny <jessnyjoseph@gmail.com>
2021-01-23 15:51:21 -0800

    changed path and fixed teamMember const

--- Develop/app.js
+++ Develop/app.js
@@ -9,7 +9,6 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
-var globalAnswers;
 var teamArray = [];
 
 
@@ -39,8 +38,9 @@ const managerPrompt = () => {
 
     ])
         .then(answers => {
-            const teamMember = new Manager(answers);
-            teamArray.push(teamMember)
+            console.log(answers);
+            const teamMember = new Manager(answers.name, answers.email, answers.id, answers.officeNumber);
+            teamArray.push(teamMember);
             newTeamMember();
         });
 
@@ -92,7 +92,8 @@ const addEngineer = () => {
         },
     ])
         .then(answers => {
-            const teamMember = addEngineer(answers);
+            console.log(answers);
+            const teamMember = addEngineer(answers.name, answers.email, answers.github);
             teamArray.push(teamMember);
             newTeamMember();
         });
@@ -118,7 +119,7 @@ const addIntern = () => {
         },
     ])
         .then(answers => {
-            const teamMember = addIntern(answers);
+            const teamMember = addIntern(answers.name, answers.email, answers.school);
             teamArray.push(teamMember);
             newTeamMember();
         });
@@ -127,16 +128,11 @@ const addIntern = () => {
 
 
 
-// function fullTeam() {
-//     console.log("Team completed!!");
-
-//     renderMain(teamArray);
-
-// }
-
-fs.writeFile('team.html', render(teamArray), (err) =>
+function fullTeam() {
+    fs.writeFile(outputPath, render(teamArray), (err) =>
     (err) ? console.error(err) : console.log('Your team is complete!!')
 );
+}
 
 
 
@@ -160,106 +156,4 @@ init();
 
 
 
-// Prompt to gather info about team members
-// const employeePrompt = () => { 
-//     return inquirer.prompt([
-//         {
-//             type: 'input',
-//             name: 'name',
-//             message: 'What is your name?',
-//         },
-//         {
-//             type: 'input',
-//             name: 'email',
-//             message: 'What is your email address?',
-//         },
-//         {
-//             type: 'input',
-//             name: 'id',
-//             message: 'What is your company id?',
-//         },
-//         {
-//             type: 'list',
-//             name: 'role',
-//             message: 'What is your title of your role in the company?',
-//             choices: ['Manager', 'Engineer', 'Intern'],
-//         },
-//     ])
-//         .then(answers => {
-//             globalAnswers = answers;
-//             if (answers.role === 'Manager') {
-//                 return inquirer.prompt([
-//                     {
-//                         type: 'input',
-//                         name: 'officeNum',
-//                         message: 'What is your office number?',
-//                     }
-//                 ])
-//             }
-//             if (answers.role === 'Engineer') {
-//                 return inquirer.prompt([
-//                     {
-//                         type: 'input',
-//                         name: 'github',
-//                         message: 'What is your Github username?',
-//                     }
-//                 ])
-//             }
-//             if (answers.role === 'Intern') {
-//                 return inquirer.prompt([
-//                     {
-//                         type: 'input',
-//                         name: 'school',
-//                         message: 'What school do you go to?',
-//                     }
-//                 ])
-//             }
 
-//         })
-//         .then(answers2 => {
-//         let fullAnswers = {
-//             ...globalAnswers,
-//             ...answers2
-//         };
-//         console.log(fullAnswers);
-//         })
-
-// };
-// employeePrompt();
-
-// using full answers to use manger classes 
-
-
-// Render Function???
-
-
-// make an empty array
-// push full answers to new array
-// call render function by passing new array as parameter
-// use fs.write file to team.html
-// mail to option in w3 schools
-
-
-// usingAnswers => (newArray) {
-//     push(fullAnswers).newArray;
-//     render();
-// }
-
-// usingAnswers();
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
